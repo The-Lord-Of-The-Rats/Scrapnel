52a55a1baa39732acdaf36c98103858a81cc66a7
kit <Aberration2025@gmail.com>
2023-06-11 14:35:26 -0500

    1

--- main.py
+++ main.py
@@ -1,51 +1,107 @@
-import time, random, sys, math, pygame
+#import time, random, sys, math, pg
 import entity
 import scavver
 #import threading
 #import colored
-
-from pygame.locals import QUIT
-
-pygame.init()
-screen = pygame.display.set_mode((680,340))
-pygame.display.set_caption('Scrapnel')
-icon = pygame.image.load("assets/download4.png")
-title = pygame.image.load("assets/titletxt.png")
-
-pygame.display.set_icon(icon)
-#pygame.display.set_caption('Hello World!')
-clock = pygame.time.Clock()
-basicdirt =  pygame.image.load("assets/bestdirt2.png")
-#basicdirt = pygame.Surface((886,886))
-
+from pg.locals import QUIT
+import pygame as pg
+import time
+import random
+#import turtle
+import sys
+import math
+pg.init()
+pg.font.init()
+class text:
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blue = '\033[94m'
+   green = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   bold = '\033[1m'
+   underline = '\033[4m'
+   END = '\033[0m'
+win = pg.display.set_mode((680, 340))
+pg.display.set_caption('Scrapnel')
+icon = pg.image.load("assets/download4.png")
+title = pg.image.load("assets/titletxt.png")
+pg.display.set_icon(icon)
+#pg.display.set_caption('Hello World!')
+clock = pg.time.Clock()
+basicdirt = pg.image.load("assets/bestdirt2.png")
+#basicdirt = pg.Surface((886,886))
 while True:
-    ## handle pygame events
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-        # or event.type == pygame.KEYUP:
-            print("Key: ",str(event.key))
-        if event.type == pygame.QUIT:
+  for event in pg.event.get():
+    if event.type == pg.KEYDOWN:
+      # or event.type == pg.KEYUP:
+        print("Key: ", str(event.key))
+        if event.type == pg.QUIT:
             #or esc
-            pygame.quit()
+            pg.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
+        if event.type == pg.KEYDOWN:
             if event.key == 27:
-                pygame.quit()
-                exit()
-        if event.type == pygame.KEYDOWN:
+              pg.quit()
+              exit()
+        if event.type == pg.KEYDOWN:
             if event.key == 9:
-                pygame.display.toggle_fullscreen()
-
-    # timed events (spawning new entities, etc)
-
-              
-    # update each entity
-
-
-    # draw screen
-    screen.blit(basicdirt,(0,0))
-    screen.blit(title,(0.2,0.2))
-  
-    pygame.display.update()
-    clock.tick(20)
-    
+                pg.display.toggle_fullwin()
+        #timed events (spawning new entities, etc)        
+        #update each entityw
+        #draw win
+        win.blit(basicdirt,(0,0))
+        win.blit(title,(0.2,0.2))
+        pg.display.update()
+        clock.tick(20)
+        while True:
+            width=win.get_width()
+            height = win.get_height()
+            mouse=pg.mouse.get_pos()
+    if showmousecors == True:
+        smalltxt(mouse,670,570)
+    global ev
+    ev = pg.event.get()
+    for event in ev:
+        if event.type == pg.KEYDOWN:
+        lastkey=event.key
+        for x in range(0, 1, 1):
+            if lastkey == pg.K_SPACE:
+                running = True
+                runtog = True
+                titl("Welcome To CritterSim v0.1")
+                Fsize = 32
+                getpet()
+        if event.key == pg.K_TAB:
+            pg.display.toggle_fullwin() 
+            global FStog
+            FStog = True
+        elif event.key == pg.K_ESCAPE:
+            pg.quit()
+            running = False
+            exit("You Quit")
+            sys.exit  
+        elif event.key == pg.K_KP_PERIOD:
+            print('Showing Logo ')
+            logov01= ("Assets\critv0.1.png")
+            window.blit(logov01, (0, 0))
+        
+        elif event.key == pg.K_LALT:
+          print('updating')
+          titl('updating...')
+          time.sleep(3)
+          titl('')
+          update()
+          srf.fill(bgclr,None,0)
+        
+        elif event.key == toggledevkey:
+        #devmode()
+          dev=true
+          print("devmode+")
+      elif event.type == pg.QUIT:
+        pg.quit()
+        running = False
+        exit("You Quit")
+        sys.exit()
+    update()
